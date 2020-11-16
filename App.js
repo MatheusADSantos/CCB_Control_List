a5ad2fbf8562111bbb0bfc675d833aad2232264b
MatheusADSantos <matheusdevmobile2020@gmail.com>
2020-11-16 13:51:21 -0300

    Implementing persistence of inputs in the cloud firestore and finishing the homescreen

--- App.js
+++ App.js
@@ -3,8 +3,14 @@ import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
+import { decode, encode } from 'base-64'
+
+import { View, ActivityIndicator } from 'react-native';
+import { firebase } from './src/firebase/config'
+
+import { user_ } from './src/screens/LoginScreen/LoginScreen';
+
+if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
@@ -14,9 +20,75 @@ export default function App() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
 
+
+
+  if(loading) {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500)
+  }
+
+  // if (loading) {	
+  //   alert('Carregando...')
+  //   return(
+  //     <View></View>
+  //   )
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000)
+  // }
+
+  // function handleStopIndicator(){
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000)
+  // }
+
+
+
+  // function handleIndicator() {
+  //   if (loading) {
+  //     return (
+  //       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //         <ActivityIndicator size='large' color='#999' />
+  //       </View>
+  //     )
+  //   } else {
+  //     setLoading(false);
+  //   }
+  // }
+
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+  console.log(">>>>>>>>>>>> ", user);
+
+  
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
+
         { user ? (
           <Stack.Screen name="Home">
             {props => <HomeScreen {...props} extraData={user} />}
@@ -27,6 +99,34 @@ export default function App() {
             <Stack.Screen name="Registration" component={RegistrationScreen} />
           </>
         )}
+
+
+
+        {/* {
+          (user) ? (
+            <Stack.Screen name="Home">
+              {props => <HomeScreen {...props} extraData={user} />}
+            </Stack.Screen>
+          ) : (
+              <>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Registration" component={RegistrationScreen} />
+              </>
+            )
+        }  */}
+
+
+
+
+{/* 
+        <Stack.Screen name="Home">
+          {props => <HomeScreen {...props} extraData={user} />}
+        </Stack.Screen>
+
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} /> */}
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,50 @@
-import * as  firebase  from  'firebase' ;
-import  '@ firebase / auth' ;
-import  '@ firebase / firestore' ;
+import * as  firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD3B24bPhWClEcQ5-OYgwVNQYKsljzSNos",
+//   authDomain: "control-covid-ccb.firebaseapp.com",
+//   databaseURL: "https://control-covid-ccb.firebaseio.com",
+//   projectId: "control-covid-ccb",
+//   storageBucket: "control-covid-ccb.appspot.com",
+//   messagingSenderId: "720193658421",
+//   appId: "1:720193658421:web:5aef359691cb672fd81b7c",
+//   measurementId: "G-VZMWVEJHR5"
+// };
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3B24bPhWClEcQ5-OYgwVNQYKsljzSNos",
-  authDomain: "control-covid-ccb.firebaseapp.com",
-  databaseURL: "https://control-covid-ccb.firebaseio.com",
-  projectId: "control-covid-ccb",
-  storageBucket: "control-covid-ccb.appspot.com",
-  messagingSenderId: "720193658421",
-  appId: "1:720193658421:web:5aef359691cb672fd81b7c",
-  measurementId: "G-VZMWVEJHR5"
+  apiKey: "AIzaSyB63ry7Lp9JcFVn7PSqch5f_x7v4C3V_JM",
+  authDomain: "ccbcontrol-7ee48.firebaseapp.com",
+  databaseURL: "https://ccbcontrol-7ee48.firebaseio.com",
+  projectId: "ccbcontrol-7ee48",
+  storageBucket: "ccbcontrol-7ee48.appspot.com",
+  messagingSenderId: "958473271620",
+  appId: "1:958473271620:web:740ad34e3564ba43927f11",
+  measurementId: "G-XSWN8T94JJ"
 };
 
-if  ( ! firebase . apps . length )  {
-    firebase . initializeApp ( firebaseConfig ) ;
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+
+  // firebase.firestore().enablePersistence()
+  //   .catch(function (err) {
+  //     if (err.code == 'failed-precondition') {
+  //       // Multiple tabs open, persistence can only be enabled
+  //       // in one tab at a a time.
+  //       // ...
+  //     } else if (err.code == 'unimplemented') {
+  //       // The current browser does not support all of the
+  //       // features required to enable persistence
+  //       // ...
+  //     }
+  //   });
+  // // Subsequent queries will use persistence, if it was enabled successfully
+
+
 }
 
-export  {  firebase  } ;
+export { firebase };
